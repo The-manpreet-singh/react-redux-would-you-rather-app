16d0650bf1ac205c4bb1038a90ad411d8a66bea6
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-24 00:38:03 +0530

    add store in index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
 
+import { createStore } from "redux";
+
+import { Provider } from "react-redux";
+
+import reducer from "./reducers";
+
+import middleware from "./middleware";
+
+const store = createStore(reducer, middleware);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
 );
-
