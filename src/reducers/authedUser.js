99ca90b32863bd5c380500124e21dcf25ffef79c
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-24 00:22:07 +0530

    add reducers authedUser.js

--- src/reducers/authedUser.js
+++ src/reducers/authedUser.js
@@ -1,6 +1,6 @@
 import { SET_AUTHED_USER } from "../actions/authedUser";
 
-export default function authedUser(state = {}, action) {
+export default function authedUser(state = null, action) {
 	switch (action.type) {
 		case SET_AUTHED_USER:
 			return action.id;
