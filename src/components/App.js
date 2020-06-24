0f9d5de9ed6344875dcbc4efa182edc240d58556
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-24 11:57:18 +0530

    import dashboard in app.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ import { connect } from "react-redux";
 
 import { handleInitialData } from "../actions/shared";
 
+import Dashboard from "./Dashboard";
+
 class App extends Component {
 	componentDidMount() {
 		this.props.dispatch(handleInitialData());
@@ -13,9 +15,16 @@ class App extends Component {
 		return (
 			<div>
 				<h1>Hello im here</h1>
+				{this.props.loading === true ? null : <Dashboard />}
 			</div>
 		);
 	}
 }
 
-export default connect()(App);
+function mapStateToProps({ authedUser }) {
+	return {
+		loading: authedUser === null,
+	};
+}
+
+export default connect(mapStateToProps)(App);
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,22 +1,27 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import {connect} from 'react-redux'
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Dashboard extends Component {
-  render() {
-    return (
-      <div>
-        <h2>My data</h2>
-        <ul>
-          {this.props.users.map((id) => (
-            <li key={id}>
-              <div>user Id: {id}</div>
-            </li>
-          ) )}
-        </ul>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<h2>My data</h2>
+				<ul>
+					{this.props.questionsIds.map((id) => (
+						<li key={id}>
+							<div>user Id: {id}</div>
+						</li>
+					))}
+				</ul>
+			</div>
+		);
+	}
 }
 
-export default connect()(Dashboard)
+function mapStateToProps({ questions }) {
+	return {
+		questionsIds: Object.keys(questions).sort((a, b) => questions[b].timestamp - questions[a].timestamp),
+	};
+}
+
+export default connect(mapStateToProps)(Dashboard);
