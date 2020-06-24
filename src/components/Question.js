44e33e28705e4b52cd44d450b2faa1cecf2579fc
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-24 13:52:46 +0530

    add question.js

---
+++ src/components/Question.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { handleAddQuestionAnswer } from '../actions/questions'
+
+class Question extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Question
