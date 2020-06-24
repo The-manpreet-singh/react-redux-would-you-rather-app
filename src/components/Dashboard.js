1c431fbb0fa6260b933ba97cf237854d0f0b148f
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-24 14:10:59 +0530

    add dashboad qustions

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,25 +2,54 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class Dashboard extends Component {
+	state = {
+		data: false,
+	};
+
+	changeHandler = (e, data) => {
+		this.setState({ data });
+	};
+
 	render() {
+		const { data } = this.state;
+		console.log(data);
 		return (
 			<div>
 				<h2>My data</h2>
-				<ul>
-					{this.props.questionsIds.map((id) => (
-						<li key={id}>
-							<div>user Id: {id}</div>
-						</li>
-					))}
-				</ul>
+				{data === false && (
+					<ul>
+						{this.props.unansweredQuestionIds.map((id) => (
+							<li key={id}>{id}</li>
+						))}
+					</ul>
+				)}
+				{data === true && (
+					<ul>
+						{this.props.answeredQuestionIds.map((id) => (
+							<li key={id}>{id}</li>
+						))}
+					</ul>
+				)}
 			</div>
 		);
 	}
 }
 
-function mapStateToProps({ questions }) {
+function mapStateToProps({ questions, authedUser }) {
 	return {
-		questionsIds: Object.keys(questions).sort((a, b) => questions[b].timestamp - questions[a].timestamp),
+		// questionsIds: Object.keys(questions).sort((a, b) => questions[b].timestamp - questions[a].timestamp),
+		answeredQuestionIds: Object.keys(questions).filter(
+			(q) =>
+				questions[q].optionOne.votes.some((c) => c === authedUser) ||
+				questions[q].optionTwo.votes.some((c) => c === authedUser)
+		),
+		unansweredQuestionIds: Object.keys(questions).filter(
+			(q) =>
+				!(
+					questions[q].optionOne.votes.some((c) => c === authedUser) ||
+					questions[q].optionTwo.votes.some((c) => c === authedUser)
+				)
+		),
 	};
 }
 
