3d6c8f9c65a86411775b0883f6446d8e67201e18
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-24 15:36:06 +0530

    add question.js

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react'
-import { handleAddQuestionAnswer } from '../actions/questions'
+import { connect } from 'react-redux'
 
 class Question extends Component {
   render() {
     return (
       <div>
-        
+        <h5>id: {Question.id}</h5>
+        <h5>author: {Question.author}</h5>
+        <h5>time: {Question.timestamp}</h5>
+        <h5>OptionOne: {Question.optionOne.text}</h5>
+        <h5>OptionTwo: {Question.optionTwo.text}</h5>
       </div>
     )
   }
 }
 
-export default Question
+const mapStateToprops = ({authedUser, users, questions, id}) => {
+  const question = questions[id]
+  return {
+    authedUser,
+    question
+  }
+}
+
+export default connect(mapStateToprops)(Question)
