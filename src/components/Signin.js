0824ccad9fcad261cc35f7736836759740f76469
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-24 12:08:23 +0530

    add signin.js

---
+++ src/components/Signin.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Signin extends Component {
+  render() {
+    return (
+      <div>
+        Signin 
+      </div>
+    )
+  }
+}
+
+
+export default Signin
