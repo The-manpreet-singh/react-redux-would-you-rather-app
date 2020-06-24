d3e05fb157ea79c5eebefbcccf5a291c107c9b0c
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-24 15:37:36 +0530

    add question.js

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,26 +1,27 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Question extends Component {
-  render() {
-    return (
-      <div>
-        <h5>id: {Question.id}</h5>
-        <h5>author: {Question.author}</h5>
-        <h5>time: {Question.timestamp}</h5>
-        <h5>OptionOne: {Question.optionOne.text}</h5>
-        <h5>OptionTwo: {Question.optionTwo.text}</h5>
-      </div>
-    )
-  }
+	render() {
+		const { question } = this.props;
+		return (
+			<div>
+				<h5>id: {question.id}</h5>
+				<h5>author: {question.author}</h5>
+				<h5>time: {question.timestamp}</h5>
+				<h5>OptionOne: {question.optionOne.text}</h5>
+				<h5>OptionTwo: {question.optionTwo.text}</h5>
+			</div>
+		);
+	}
 }
 
-const mapStateToprops = ({authedUser, users, questions, id}) => {
-  const question = questions[id]
-  return {
-    authedUser,
-    question
-  }
-}
+const mapStateToprops = ({ authedUser, users, questions, id }) => {
+	const question = questions[id];
+	return {
+		authedUser,
+		question,
+	};
+};
 
-export default connect(mapStateToprops)(Question)
+export default connect(mapStateToprops)(Question);
