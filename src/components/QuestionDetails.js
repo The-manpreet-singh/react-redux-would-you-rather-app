549afe7cac9668d9964a0fd2516b23183d97f477
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-25 21:39:53 +0530

    add QuestionDetails.js

--- src/components/QuestionDetails.js
+++ src/components/QuestionDetails.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Question from "./Question";
 
 class QuestionDetails extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+	render() {
+		const { id } = this.props;
+		return (
+			<div>
+				<Question id={id} />
+			</div>
+		);
+	}
 }
 
-export default QuestionDetails;
+function mapStateToProps(props) {
+	const { id } = props.match.params;
+
+	return {
+		id,
+	};
+}
+
+export default connect(mapStateToProps)(QuestionDetails);
