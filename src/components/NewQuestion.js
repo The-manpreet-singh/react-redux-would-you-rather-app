32e35d2131cf41cb53483588789ce4d48aaa5994
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-25 15:17:25 +0530

    add newQuestion.js

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,14 +1,47 @@
-
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { handleAddQuestion } from "../actions/questions";
 
 class NewQuestion extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+	state = {
+		optionOne: "",
+		optionTwo: "",
+	};
+
+	changehandler = (event, option) => {
+		const input = e.target.value;
+
+		this.setState(() => ({
+			[option]: input,
+		}));
+	};
+
+	submitHandler = (event) => {
+		event.preventDefault();
+		const { optionOne, optionTwo } = this.state;
+		const { dispatch } = this.props;
+
+		dispatch(handleAddQuestion(optionOne, optionTwo));
+
+		this.setState(() => ({
+			optionOne,
+			optionTwo,
+		}));
+	};
+
+	render() {
+		const { optionOne, optionTwo } = this.state;
+
+		return (
+			<div>
+				<form onSubmit={this.submitHandler}>
+					<input type="text" onChange={(e) => this.handleChange("optionOne", e)} defaultValue={optionOne} />
+					<input type="text" onChange={(e) => this.handleChange("optionTwo", e)} defaultValue={optionTwo} />
+					<button type="submit">ADD</button>
+				</form>
+			</div>
+		);
+	}
 }
 
-export default NewQuestion
+export default connect(NewQuestion);
