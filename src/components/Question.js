8107771d82c4604341d1aaa65389f7ebb95720d3
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-25 22:15:37 +0530

    modified question.js

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,28 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { handleAddQuestionAnswer } from "../actions/questions";
 import { Link, withRouter } from "react-router-dom";
 
 class Question extends Component {
+	state = {
+		selected: "",
+	};
+
+	optionSelectHandler = (value) => {
+		this.setState(() => ({
+			selected: value,
+		}));
+	};
+
+	submitHandler = (event) => {
+		event.preventDefault();
+		const { selected } = this.state;
+		const { dispatch, id } = this.props;
+
+		dispatch(handleAddQuestionAnswer(id, selected));
+	};
 	render() {
-		const { question, author, id, authedUser, detailed } = this.props;
+		const { authedUserDetails, question, author, id, authedUser, detailed } = this.props;
 
 		if (question === null) {
 			return <div>Not exists</div>;
@@ -15,21 +33,33 @@ class Question extends Component {
 				<h5>{detailed ? <span>{question.id}</span> : <Link to={`/question/${id}`}>{question.id}</Link>}</h5>
 				<h5>author: {author.name}</h5>
 				<h5>time: {question.timestamp}</h5>
-				{question.optionOne.votes.filter((v) => v === authedUser).length ||
-				question.optionTwo.votes.filter((v) => v === authedUser).length ? (
+				{authedUserDetails.answers[question.id] ? (
 					<span>
 						Selected:
 						{question.optionOne.votes.filter((v) => v === authedUser).length
 							? question.optionOne.text
 							: question.optionTwo.text}
+						{question[authedUserDetails.answers[question.id]].text}
 					</span>
 				) : (
 					<span>
 						{detailed ? (
-							<form>
-								<input type="radio" name="gender" id="optionone" value="male" />
+							<form onSubmit={this.handleSubmit}>
+								<input
+									type="radio"
+									name="gender"
+									id="optionone"
+									value="optionOne"
+									onChange={(e) => this.optionSelectHandler(e.currentTarget.value)}
+								/>
 								<label htmlFor="optionone">{question.optionOne.text}</label>
-								<input type="radio" name="gender" id="optiontwo" value="female" />
+								<input
+									type="radio"
+									name="gender"
+									id="optiontwo"
+									value="optionTwo"
+									onChange={(e) => this.optionSelectHandler(e.currentTarget.value)}
+								/>
 								<label htmlFor="optiontwo">{question.optionTwo.text}</label>
 								<button type="submit">Submit</button>
 							</form>
@@ -43,11 +73,13 @@ class Question extends Component {
 
 const mapStateToProps = ({ authedUser, questions, users }, { id }) => {
 	const question = questions[id];
+	const authedUserDetails = users[authedUser];
 	const author = users[question.author];
 	return {
 		authedUser,
 		question,
 		author,
+		authedUserDetails,
 	};
 };
 
