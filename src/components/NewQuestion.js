235e9fe4077d8d3051580b2f43d00619afb9a598
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-27 13:15:59 +0530

    set style for new question

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -40,11 +40,31 @@ class NewQuestion extends Component {
 		}
 
 		return (
-			<div>
-				<form onSubmit={this.submitHandler}>
-					<input type="text" onChange={(e) => this.changeHandler(e, "optionOne")} defaultValue={optionOne} />
-					<input type="text" onChange={(e) => this.changeHandler(e, "optionTwo")} defaultValue={optionTwo} />
-					<button type="submit">ADD</button>
+			<div className="new-question-form">
+				<h2>Create a New question</h2>
+				<h5>Would you rather...</h5>
+				<form className="ui form" onSubmit={this.submitHandler}>
+					<div className="field">
+						<label>Option One</label>
+						<input
+							type="text"
+							placeholder="Enter option one text"
+							onChange={(e) => this.changeHandler(e, "optionOne")}
+							defaultValue={optionOne}
+						/>
+					</div>
+					<div className="field">
+						<label>Option Two</label>
+						<input
+							type="text"
+							placeholder="Enter option two text"
+							onChange={(e) => this.changeHandler(e, "optionTwo")}
+							defaultValue={optionTwo}
+						/>
+					</div>
+					<button type="submit" className="ui blue button">
+						ADD QUESTION
+					</button>
 				</form>
 			</div>
 		);
